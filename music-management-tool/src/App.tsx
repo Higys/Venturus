651181724e24047828fda74ff8275af375bf5a4e
Higys <igor.yaro@gmail.com>
2024-02-05 23:16:48 -0300

    configured application context, added dialog and delete function

--- music-management-tool/src/App.tsx
+++ music-management-tool/src/App.tsx
@@ -3,10 +3,14 @@ import { Router } from "./Router";
 import { BrowserRouter } from "react-router-dom";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { initialState, musicReducer } from "./store/musicReducer";
+import { StoreProvider } from "./store/store";
+
 
 export function App() {
   
   return (
+    <StoreProvider initialState={initialState} reducer={musicReducer}>
       <ThemeProvider theme={defaultTheme}>
         <BrowserRouter>
           <Router/>
@@ -14,5 +18,6 @@ export function App() {
         </BrowserRouter>
         <GlobalStyle/>
       </ThemeProvider>
+    </StoreProvider> 
   )
 }
